a5fd10ad9f0e006458f1bfb243e5948d7fb12b4d
Maximilian Tschulik <maxi.tschulik@gmail.com>
2025-04-30 17:51:07 +0200

    Cap distance term in gravitation calculations

--- src/force/GravitationalForce.js
+++ src/force/GravitationalForce.js
@@ -21,7 +21,9 @@ export class GravitationalForce extends ForceSource {
         let gravityCenter = [this.x, this.y];
         let particlePosition = [flyingObject.x, flyingObject.y];
         let distance = math.distance(gravityCenter, particlePosition);
-        let magnitude = (this.strength * flyingObject.motionState.mass) / (distance * distance + 1e-8);
+        // Note: the distance term in the denominator is capped to avoid divisions by zero and extreme acceleration when
+        // objects get close to the gravitational center
+        let magnitude = (this.strength * flyingObject.motionState.mass) / Math.max(distance * distance, 150);
         let direction = math.subtract(gravityCenter, particlePosition);
         direction = math.divide(direction, math.norm(direction));
 
